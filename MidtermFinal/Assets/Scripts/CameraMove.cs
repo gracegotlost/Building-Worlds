b2c22031b4362aa8745114ec0905f70b6aba579d
gracegotlost <lopkve@gmail.com>
2014-12-21 03:20:42 -0500

    add interaction

---
+++ MidtermFinal/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	public float offsetMove = 10.0f;
+
+	// MOUSE LOOK
+	public bool isMoving = false;
+	
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX;
+	public float sensitivityY;
+	
+	public float minimumX;
+	public float maximumX;
+	
+	public float minimumY;
+	public float maximumY;
+	
+	float rotationY = 0F;
+	// END OF MOUSE LOOK
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// ARROW KEY
+		if (Input.GetKey (KeyCode.A) ) {
+			if (transform.position.x > 998f) {
+				rigidbody.AddForce(-transform.right * offsetMove);
+			}
+		} else if (Input.GetKey (KeyCode.D) ) {
+			if (transform.position.x < 1000f) {
+				rigidbody.AddForce(transform.right * offsetMove);
+			}
+		} else if (Input.GetKey (KeyCode.W) ) {
+			if (transform.position.z < 509.4f) {
+				rigidbody.AddForce(transform.forward * offsetMove);
+			}
+		} else if (Input.GetKey (KeyCode.S) ) {
+			if (transform.position.z > 508.4f) {
+				rigidbody.AddForce(-transform.forward * offsetMove);
+			}
+		}
+		// END OF ARROW KEY
+
+		// RAYCASTING
+		// generate a ray based on camera position + mouse cursor screen coordinate
+		Ray ray = Camera.main.ScreenPointToRay ( Input.mousePosition );
+
+		// reserve space for info about where the raycast hit a thing, what it hit, etc.
+		RaycastHit rayHit = new RaycastHit(); // initialize forensics data container
+		
+		// actually shoot the raycast
+		if (Physics.Raycast (ray, out rayHit, 1000f) 
+						&& Input.GetMouseButton (0) 
+		    			&& rayHit.collider.CompareTag ("PullUp") ) {
+						isMoving = false;
+						//Debug.Log ("Can't Move!");
+				} else if (Input.GetMouseButton (0)) {
+						isMoving = true;	
+						//Debug.Log ("Can Move!");
+				} else {
+			isMoving = false;
+			//Debug.Log("Can't Move!");
+		}
+		// END OF RAYCASTING
+
+		// MOUSE LOOK
+		// Wrap bool in if statement so can set to false when GUI screen pops up
+		if (isMoving == true) {
+			
+			if (axes == RotationAxes.MouseXAndY) {
+				float rotationX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * sensitivityX;
+				
+				rotationY += Input.GetAxis ("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				
+				transform.localEulerAngles = new Vector3 (-rotationY, rotationX, 0);
+			} else if (axes == RotationAxes.MouseX) {
+				transform.Rotate (0, Input.GetAxis ("Mouse X") * sensitivityX, 0);
+			} else {
+				rotationY += Input.GetAxis ("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				
+				transform.localEulerAngles = new Vector3 (-rotationY, transform.localEulerAngles.y, 0);
+			}
+		}
+		// END OF MOUSE LOOK
+
+	}
+	
+}
---
+++ MidtermFinal/Assets/Scripts/PullUp.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PullUp : MonoBehaviour {
+
+	public float offset;
+
+	float pmouseY;
+	GameObject pullup;
+	GameObject pole;
+	GameObject basePole;
+	GameObject topPole;
+
+	// Use this for initialization
+	void Start () {
+		pullup = GameObject.Find ("/PullUp");
+		pole = GameObject.Find ("/Pole");
+		basePole = GameObject.Find("/PoleHiddenBase");
+		topPole = GameObject.Find ("/PullUp/PoleHiddenTop");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pmouseY = Input.mousePosition.y;
+	}
+
+	void OnMouseDrag() {
+		float temp = (topPole.transform.position.y + basePole.transform.position.y) * 0.5f;
+		float tempScale = Mathf.Sqrt((temp - 2.76f) * 10f);
+
+		if (temp >= 3f && temp <= 4.4f){
+			pullup.transform.position += new Vector3 (0,( Input.mousePosition.y - pmouseY ) * Time.deltaTime * offset, 0);
+
+			pole.transform.position = new Vector3 (pole.transform.position.x, 
+			                                       temp, 
+			                                       pole.transform.position.z);
+			pole.transform.localScale = new Vector3 ( tempScale, 
+			                                         pole.transform.localScale.y, 
+			                                         pole.transform.localScale.z);
+			//Debug.Log (pole.transform.localScale.x);
+			//Debug.Log (Input.mousePosition.y - pmouseY);
+			//Debug.Log ("Is dragging!");
+		} else if (temp < 3f) {
+			pullup.transform.position = new Vector3 (pullup.transform.position.x, 
+			                                         4.44f,
+			                                         pullup.transform.position.z);
+		} else if (temp > 4.4f) {
+			pullup.transform.position = new Vector3 (pullup.transform.position.x, 
+			                                          7.24f,
+			                                          pullup.transform.position.z);
+		}
+	}
+}
